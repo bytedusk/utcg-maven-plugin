b15b30c9618a16bdcef3829ad13785855975d802
bytedusk <klxx@outlook.com>
2024-05-29 21:10:42 +0800

    调整结构

--- src/main/java/com/bytedusk/dev/plugin/maven/utcg/JavaParserMain.java
+++ src/main/java/com/bytedusk/dev/plugin/maven/utcg/JavaParserMain.java
@@ -1,23 +1,19 @@
-package com.bytedusk.dev.plugin.maven.utcg.demo;
+package com.bytedusk.dev.plugin.maven.utcg;
 
 import com.github.javaparser.ParserConfiguration;
-import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.resolution.TypeSolver;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import com.github.javaparser.utils.CodeGenerationUtils;
 import com.github.javaparser.utils.SourceRoot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class JavaParserMain {
     public static void main(String args[]){
@@ -31,36 +27,30 @@ public class JavaParserMain {
 
         sourceRoot.setParserConfiguration(parserConfiguration);
         // Our sample is in the root of this directory, so no package name.
-        CompilationUnit cu = sourceRoot.parse("com.bytedusk.dev.plugin.maven.utcg.demo", "RessCryptoUtils.java");
+        CompilationUnit cu = sourceRoot.parse("com.bytedusk.dev.plugin.maven.utcg", "TestMojo.java");
         cu.getImports().forEach(item -> System.out.println(item.getName()));
-
         cu.getTypes().forEach(typeDeclaration -> System.out.println(typeDeclaration.getName()));
 
         //field
-        Map<String, Type> fieldMap = new HashMap<>();
+        List<FieldDeclaration> fdl = new ArrayList<FieldDeclaration>();
         cu.accept(new VoidVisitorAdapter<Object>() {
             public void visit(final FieldDeclaration f, final Object arg) {
-                f.getVariables().forEach(var ->{
-                    fieldMap.put(var.getNameAsString(), var.getType());
+                fdl.add(f);
+                System.out.println(f.getVariables().getFirst());
+                System.out.println(f.getAnnotations().getFirst());
+            }
+        } , null );
+
+        //method
+        List<MethodDeclaration> mdl = new ArrayList<MethodDeclaration>();
+        cu.accept(new VoidVisitorAdapter<Object>() {
+            public void visit(final MethodDeclaration m, final Object arg) {
+                mdl.add(m);
+                System.out.println(m.getName()+":"+m.getType()+":"+m.getParameters());
+                m.findAll(MethodCallExpr.class).forEach(mce ->{
+                    System.out.println(mce.getScope()+":"+mce.getName()+":"+mce.resolve().getQualifiedSignature());
                 });
             }
         } , null );
-
-        fieldMap.forEach((k,v)->System.out.println(k+":"+v+":"+v.getMetaModel()));
-
-        ArrayList<MethodDeclaration> mdl = new ArrayList<>();
-        cu.accept(new VoidVisitorAdapter<Object>() {
-            public void visit(final MethodDeclaration m, final Object arg) {
-                System.out.println(m.getName()+":"+m.getType()+":"+m.getParameters());
-                mdl.add(m);
-            }
-        } , null );
-
-        mdl.forEach( m -> {
-            m.findAll(MethodCallExpr.class).forEach(mce ->{
-                        System.out.println(mce.getScope()+":"+mce.getName()+":"+mce.resolve().getQualifiedSignature());
-                    });
-        });
-
     }
 }
